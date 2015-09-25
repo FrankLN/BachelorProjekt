3b460626ef8df485c090794f6e1004e2ef78b4c4
Lea Slemming-Adamsen <Lea@Leas-MacBook-Air.local>
2015-09-25 12:18:37 +0200

    graph update

---
+++ WebApp/WebApp/Content/script/chartScript.js
@@ -0,0 +1,27 @@
+﻿function drawGraph() {
+    $('#container').highcharts({
+        data: {
+            table: 'datatable'
+        },
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Graph over episodetype'
+        },
+        yAxis: {
+            allowDecimals: false,
+            title: {
+                text: 'Transmissions'
+            }
+        },
+        tooltip: {
+            formatter: function () {
+                return '<b>' + this.series.name + '</b><br/>' +
+                    this.point.y + ' ' + this.point.name.toLowerCase();
+            }
+        }
+    });
+
+    
+}
--- WebApp/WebApp/Content/script/popupScript.js
+++ WebApp/WebApp/Content/script/popupScript.js
@@ -1,5 +1,16 @@
-﻿function overlay() {
+﻿function overlay(typeName, count) {
+    if (typeName != 'close') {
+        var graphHead = document.getElementById('graphHead');
+        var graphData = document.getElementById('graphData');
+        console.log(graphData);
+        graphHead.innerHTML = "<tr><th></th><th>" + typeName + "</th></tr>";
+        graphData.innerHTML = "<tr><th>" + typeName + "</th><td>" + count + "</td></tr>";
+
+
+        drawGraph();
+    }
     el = document.getElementById("overlay");
     el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
 }
+
 
