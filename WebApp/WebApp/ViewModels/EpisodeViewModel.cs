547232e6c98167779ac1ed5c5f3fb6aa786071a7
Frank Laumann Nielsen <frankder@hotmail.com>
2015-09-30 10:19:53 +0200

    ViewModel Updated

--- WebApp/WebApp/ViewModels/EpisodeViewModel.cs
+++ WebApp/WebApp/ViewModels/EpisodeViewModel.cs
@@ -9,30 +9,42 @@ namespace WebApp.ViewModels
     public class EpisodeViewModel
     {
         [DisplayName("Episode type")]
-        [Tooltip("The type of episode")]
+        [Helpers.Tooltip("The type of episode")]
         public String EpisodeType { get; set; }
 
-        [DisplayName("Transmission")]
-        [Tooltip("The number of Transmissions where the episode type is present")]
-        public int Transmissions { get; set; }
-
-        [DisplayName("Transmission %")]
-        [Tooltip("The procent of all transmissions where the episode is present")]
-        public int ProcentTransmission { get; set; }
-    }
-
-    public class TooltipAttribute : DescriptionAttribute
-    {
-        public TooltipAttribute()
-            : base("")
+        private int transmission;
+        [DisplayName("Transmissions")]
+        [Helpers.Tooltip("The number of Transmissions where the episode type is present")]
+        public int Transmissions
         {
-
+            get
+            {
+                return transmission;
+            }
+            set
+            {
+                if(value >= 0)
+                    transmission = value;
+                else
+                    throw new ArgumentOutOfRangeException();
+            }
         }
 
-        public TooltipAttribute(string description)
-            : base(description)
-        {
-
+        private int procentTransmission;
+        [DisplayName("Transmission %")]
+        [Helpers.Tooltip("The procent of all transmissions where the episode is present")]
+        public int ProcentTransmission { 
+            get
+            {
+                return procentTransmission;
+            }
+            set
+            {
+                if (value >= 0 && value <= 100)
+                    procentTransmission = value;
+                else
+                    throw new ArgumentOutOfRangeException();
+            }
         }
     }
 }
