7e9de4101623962e77c0242e60341f015f7f6162
Lea Slemming-Adamsen <Lea@Leas-MacBook-Air.local>
2015-11-04 08:41:13 +0100

    uc4&uc5

---
+++ WebApp/WebApp/Content/script/dropdownScript.js
@@ -0,0 +1,9 @@
+﻿$(function () {
+    $(".dropdown").click(function () {
+        $(this).toggleClass("is-active");
+    });
+
+    $(".dropdown ul").click(function (e) {
+        e.stopPropagation();
+    });
+});
