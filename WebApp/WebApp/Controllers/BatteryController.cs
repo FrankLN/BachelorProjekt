b32366f1fccc0d27345b2255700eb41e5a5df3dc
Lea Slemming-Adamsen <Lea@Leas-MacBook-Air.local>
2015-11-14 15:12:06 +0100

    layout fil

---
+++ WebApp/WebApp/Controllers/BatteryController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class BatteryController : Controller
+    {
+        private Models.pacemakerdataEntities dbModel = new Models.pacemakerdataEntities();
+
+        public BatteryController()
+        {
+            try
+            {
+                var test = dbModel.batteristatistikview.Count();
+            }
+            catch (Exception e)
+            {
+                ISet<Models.batteristatistikview> test = new HashSet<Models.batteristatistikview>();
+                dbModel.batteristatistikview = test as System.Data.Entity.DbSet<Models.batteristatistikview>;
+            }
+        }
+
+        // GET: Home
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var model = new ViewModels.BatteryGraphViewModel();
+            model.ByYear = new List<int>();
+            model.ByMonth = new List<int>();
+            model.MonthName = new List<string>();
+
+            var date = DateTime.Now;
+
+            for (var i = 0; i < 12; i++)
+            {
+                model.ByYear.Add(0);
+                model.ByMonth.Add(0);
+                model.MonthName.Add(date.AddMonths(i).ToString("MMMM yyyy"));
+            }
+
+            var thisYear = date.Year;
+            var thisMonth = date.Month;
+
+            if(dbModel.batteristatistikview != null)
+            {
+                foreach(var row in dbModel.batteristatistikview.OrderBy(m => m.estimatedDaysOfServiceLeft))
+                {
+                    var tempDate = date.AddDays(row.estimatedDaysOfServiceLeft);
+
+                    model.ByYear[tempDate.Year - thisYear]++;
+                    if (tempDate.Year == thisYear || tempDate.Year == thisYear + 1)
+                        model.ByMonth[tempDate.Month - thisMonth < 0 ? tempDate.Month - thisMonth + 12 : tempDate.Month - thisMonth]++;
+                }
+            }
+            ViewBag.CurYear = thisYear;
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public JsonResult getBatteryData()
+        {
+            Dictionary<string, int> result = new Dictionary<string, int>();
+            var dateNow = DateTime.Now;
+
+            if(dbModel.batteristatistikview != null)
+            {
+                foreach(var modelItem in dbModel.batteristatistikview.OrderBy(m => m.estimatedDaysOfServiceLeft))
+                {
+                    var key = dateNow.AddDays(modelItem.estimatedDaysOfServiceLeft).ToString("dd-MM-yyyy");
+
+                    if (result.ContainsKey(key))
+                    {
+                        result[key]++;
+                    }
+                    else
+                    {
+                        result.Add(key, 1);
+                    }
+
+                }
+            }
+
+            var resultJS = new List<ViewModels.BatteryJSViewModel>();
+
+            foreach(var obj in result)
+            {
+                resultJS.Add(new ViewModels.BatteryJSViewModel() { date = obj.Key, value = obj.Value });
+            }
+
+            return Json(resultJS, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ WebApp/WebApp/ViewModels/BatteryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp.ViewModels
+{
+    public class BatteryViewModel
+    {
+        public string PatientName { get; set; }
+        public string DateOfImplantation { get; set; }
+        public int PacemakerSerialNumber { get; set; }
+        public int EstimatedDaysOfServiceLeft { get; set; }
+    }
+}
